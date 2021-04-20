4c91fb42fb3be2087735d77bb15cd048f0ddbc9e
gaston <litoralcreatives@gmail.com>
2021-04-20 10:46:29 -0300

    third commit

--- app.js
+++ app.js
@@ -12,3 +12,20 @@ function changeAlign(index) {
   let container = document.getElementById("container");
   container.style.alignItems = index;
 }
+
+function wrap(index) {
+  let container = document.getElementById("container");
+  container.style.flexWrap = index;
+}
+
+function removeItem() {
+  let container = document.getElementById("container");
+  container.removeChild(container.firstChild);
+}
+
+function addItem() {
+  let container = document.getElementById("container");
+  const newItem = document.createElement("div");
+  newItem.className = "box";
+  container.appendChild(newItem);
+}

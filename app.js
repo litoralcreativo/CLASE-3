04b15610a908198475e3b4a492df0fd92ac60693
gaston <litoralcreatives@gmail.com>
2021-04-20 10:10:55 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+function changeJustify(index) {
+  let container = document.getElementById("container");
+  container.style.justifyContent = index;
+}
+
+function chengeDirection(index) {
+  let container = document.getElementById("container");
+  container.style.flexDirection = index;
+}
+
+function changeAlign(index) {
+  let container = document.getElementById("container");
+  container.style.alignItems = index;
+}
